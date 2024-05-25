62902cb6c4aadb0c0cd7492415433166edbdc85f
leorvergani <vergani@outlook.com.br>
2024-05-24 21:18:52 -0300

    treinamento de JS

---
+++ index.js
@@ -0,0 +1,13 @@
+const id = 26;
+let username = 'Luis';
+
+if(25 == id || 'Luiz' == username){
+  console.log("Quer alterar sua senha?")
+} else {
+  console.log("Usuário sem autorização.")
+}
+
+let numeros = [2, 5, 7, 10]
+console.log(numeros)
+
+let usuario
